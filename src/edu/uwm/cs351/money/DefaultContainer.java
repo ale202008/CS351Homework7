e847d152b700a69c842fbc4ee105ede77045db6e
Proje <Proje@AsteriskLe>
2022-10-30 22:46:00 -0500

    Worked on invariant for Stack. Hasn't worked much so rip.

--- src/edu/uwm/cs351/money/DefaultContainer.java
+++ src/edu/uwm/cs351/money/DefaultContainer.java
@@ -114,9 +114,6 @@ public class DefaultContainer implements Container {
 			return true;
 		}
 		
-
-		
-		
 		return false;
 	}
 
--- src/edu/uwm/cs351/money/Stack.java
+++ src/edu/uwm/cs351/money/Stack.java
@@ -7,4 +7,45 @@ package edu.uwm.cs351.money;
 public class Stack extends DefaultContainer {
 	// TODO
 	// Add the least amount that gives the required semantics.
+	protected boolean wellFormed() {
+		
+		if (!super.wellFormed()) {
+			return false;
+		}
+		
+		for (Coin c = head; c != null && c.next != null; c = c.next) {
+			if (c.type.getValue() > c.next.type.getValue()) {
+				return report("coin cannot be placed on smaller ones.");
+			}
+		}
+		return true;
+	}
+	
+//	@Override // implementation
+//	public void add(Coin c) {
+//		// TODO Auto-generated method stub
+//		assert wellFormed() : "invariant failed at start of Stackadd";
+//		
+//		
+//		if (!canAdd(c)) {
+//			takeOwnership(c);
+//			throw new IllegalArgumentException();
+//		}
+//		else {
+//			takeOwnership(c);
+//			
+//			Coin i;
+//			for (i = head; head != null; i = i.next) {
+//				if (i.type.getValue() >= c.type.getValue()) {
+//					break;
+//				}
+//			}
+//			c.next = i.next;
+//			i.next = c;
+//
+//		}
+//		
+//		assert wellFormed() : "invariant failed at end of Stackadd";
+//		
+//	}
 }
