cf316713851b9c931a6225a8d1cd1d3e7b674a92
Proje <Proje@AsteriskLe>
2022-10-31 01:28:57 -0500

    Began working on Pipeline. Invariants for wellFormed implemented.

--- src/edu/uwm/cs351/money/Pipeline.java
+++ src/edu/uwm/cs351/money/Pipeline.java
@@ -7,4 +7,16 @@ public class Pipeline extends DefaultContainer {
 	protected Coin tail; // Add only this field
 	
 	// TODO: Add what is necessary to get correct semantics
+	
+	protected boolean wellFormed() {
+		if (!super.wellFormed()) {
+			return false;
+		}
+		
+		if (tail.next != null) {
+			return report("tail is not the last coin in the Pipeline.");
+		}
+		
+		return true;
+	}
 }
